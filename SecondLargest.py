737b7384014c0cabb99b27373fa23f216eb51cd6
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2020-09-25 21:36:06 +0700

    update algorithm

--- SecondLargest.py
+++ SecondLargest.py
@@ -1,15 +1,17 @@
 def secondLargest(ar):
-    if (len(ar)) == 0:
+    if +len(ar) == 0:
         l = 0
         l2 = 0
         pass
+    elif len(ar) == 1:
+      return ar[0]
     else:
-        l = ar[0]
-        l2 = ar[0]
-        for n in range(0, len(ar) - 1):
+        l = 0
+        l2 = 0
+        for n in range(0, len(ar)):
             if l < ar[n]:
                 l = ar[n]
-        for i in range(0, len(ar) - 1):
+        for i in range(0, len(ar)):
             if l2 < ar[i] < l:
                 l2 = ar[i]
         return l2
@@ -26,5 +28,5 @@ while True:
         exit(-1)
     elif len(reader) == 0:
         break
-print(str(ray))
+print("Given array: " + str(ray))
 print("Second largest value in array: " + str(secondLargest(ray)))
