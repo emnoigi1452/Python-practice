b75c5ef29b156f02782758a36c586bef475eb426
emnoigi1452 <63104132+emnoigi1452@users.noreply.github.com>
2021-10-14 09:11:09 +0700

    yea

--- SingleLinePrime.py
+++ SingleLinePrime.py
@@ -1,2 +1,3 @@
 import math
-prime = lambda num: [num % i == 0 for i in range(2, int(math.sqrt(num))+1)].count(True) == 0
+prime = lambda num: [num % i == 0 for i in range(2, int(math.sqrt(num))+1)].count(True) == 0 and num >= 2
+# do like some inputs later on to test it idk
