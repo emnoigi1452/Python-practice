ad157e3132519dd39b37e7b5071f2e701da05906
Stella (DucTrader) <63104132+emnoigi1452@users.noreply.github.com>
2021-11-19 08:48:58 +0700

    Some basic quality of life changes (Read desc.)
    (*) Vietnamese (vi)
    [+] Chỉ hiện thị tới lớp electron ngoài cùng
    [+] Hiển thị số lớp, số electron lớp ngoài cùng
    [+] Tạo ngăn cách giữa các lần nhập
    (*) English (en)
    [+] Only displays electron shells till the outermost shell
    [+] Added display for shell count and the number of electrons of the last shell
    [+] Added "barrier" between each user input, so you can read the output easily

--- Electrons.py
+++ Electrons.py
@@ -1,11 +1,13 @@
 sublimits = dict({'s': 2, 'p': 6, 'd': 10, 'f': 14})
 scripts = '¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ¹⁰ ¹¹ ¹² ¹³ ¹⁴'.split(' ')
-print('Ghi chú: Hãy nhấn -1 nếu bạn không muốn tiếp tục!')
+print('\nGhi chú: Hãy nhấn -1 nếu bạn không muốn tiếp tục!')
+print("-------------------------------------------------")
 while True:
    e = int(input('Nhập vào số electron: ')); charge = e
    if e < 0: break
-   configuration = ''
+   configuration = ''; shells = 0; outter = 0
    shell_info = dict({1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0})
+   shell_id = dict({1: 'K', 2: 'L', 3: 'M', 4: 'N', 5: 'O', 6: 'P', 7: 'Q'})
    sequence = '1s, 2s, 2p, 3s, 3p, 4s, 3d, 4p, 5s, 4d, 5p, 6s, 4f, 5d, 6p, 7s, 5f, 6d, 7p'.split(', ')
    for level in sequence:
       t = level[1]; limit = sublimits[t]; layer = int(level[0]); addon = 0
@@ -15,14 +17,17 @@ while True:
       e -= addon; shell_info.update({layer: shell_info[layer]+addon})
       if e < 1:
          break
-   print('Số electron: %s' % (str(charge)))
    print('Cấu hình của electron: %s' % (configuration))
    print('Các lớp electron:')
-   print('  - K: %s' % (str(shell_info[1])))
-   print('  - L: %s' % (str(shell_info[2])))
-   print('  - M: %s' % (str(shell_info[3])))
-   print('  - N: %s' % (str(shell_info[4])))
-   print('  - O: %s' % (str(shell_info[5])))
-   print('  - P: %s' % (str(shell_info[6])))
-   print('  - Q: %s' % (str(shell_info[7])))
+   for key in shell_info.keys():
+       shell_name = shell_id[key]; shell_count = shell_info[key]
+       if shell_count == 0: break
+       else:
+           print('   %s: %d' % (shell_name, shell_count))
+           shells += 1; outter = shell_count
+
+   print("Số lớp electron = %d" % (shells,))
+   print("Số electron lớp ngoài cùng = %d" % (outter,))
+   print("--------------------------------------")
+
 print('Cảm ơn đã dùng :) Mong bạn thích! Made by Đức Hải Cẽo!')
